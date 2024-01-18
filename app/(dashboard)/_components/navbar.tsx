c067450dc214bbb959a50ca20e7e62d4e1923cb5
luisalbeto <luis-alberto11@hotmail.com>
2024-01-17 22:21:36 -0400

    first commit portafoliowow

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,24 @@
+
+
+export const Navbar = () => {
+  return(
+    <div
+    className=" h-fit
+    bg-gradient-to-r
+    from-sky-800
+    p-6
+    justify-center
+    text-2xl
+    text-yellow-500
+    fixed w-full 
+    bg-black
+    font-altivo
+    
+    
+"
+    >
+      Portafolios Wow
+      </div>
+  
+  )
+}
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "./_components/navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>
+        Hello
+      </h1>
+
+
+
+    </div>
+    
+  )
+}
